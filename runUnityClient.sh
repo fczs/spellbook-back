c3e2a620526e277105987e1e7678497095c97f03
Ivan Kuzin <corruptsouls@gmail.com>
2021-01-11 21:40:02 +0300

    refactor feed-connector

--- runUnityClient.sh
+++ runUnityClient.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CLASSPATH="wrapper.jar:./conf/:../libs/*:"
+CLASSPATH="wrapper.jar:./conf/:./libs/*:"
 echo "$CLASSPATH" > cp.txt
 
 java -cp "$CLASSPATH" -Xmx1024m -Dlogback.configurationFile="./conf/logback.xml" -Dhost="127.0.0.1" -Dport="8992" jayeson.service.feedwrapper.client.Main &
